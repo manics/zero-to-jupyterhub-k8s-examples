4f391c21df8454042b876c65a6ad7b9762b78b63
Simon Li <orpheus+devel@gmail.com>
2022-10-07 17:32:34 +0100

    Add browser (playwright) LDAP test

---
+++ ldap-singleuser/tests/test_jupyterhub_ldap.py
@@ -0,0 +1,21 @@
+# Run this test with Playwright Pytest args:
+# https://playwright.dev/python/docs/test-runners
+#
+# pytest --browser firefox --base-url http://%K8S_HOSTNAME% [--headed --slowmo 1000]
+
+
+def test_browser(page):
+    # Go to <base-url>/, wait for redirect to login page
+    page.goto("/")
+    page.wait_for_url("/hub/login?next=%2Fhub%2F")
+
+    # Fill username and password
+    page.locator('input[name="username"]').fill("zoidberg")
+    page.locator('input[name="password"]').fill("zoidberg")
+
+    # Click "Sign in"
+    page.locator('input:has-text("Sign in")').click()
+    # page.wait_for_url("/hub/spawn-pending/*")
+
+    # Server should be spawned, and should be redirected to JupyterLab
+    page.wait_for_url("/user/zoidberg/lab")
