a46caa074503bbbd9f36d4568b3150d8cbc0e192
Simon Li <orpheus+devel@gmail.com>
2025-12-19 23:37:07 +0000

    run_codeblocks.py: run multiline code block as single script
    This allows state such as variables to be passed in a command

--- ci/run_codeblocks.py
+++ ci/run_codeblocks.py
@@ -2,7 +2,10 @@
 # Extract and run fenced codeblocks from a markdown file
 
 from argparse import ArgumentParser
+from os import getenv
 from subprocess import run
+
+CI = getenv("CI") == "true"
 
 parser = ArgumentParser(description="Extract fenced codeblocks from a markdown file")
 parser.add_argument("input", help="Input markdown file")
@@ -16,20 +19,39 @@ parser.add_argument("--run", action="store_true", help="Run the code")
 
 args = parser.parse_args()
 
-code = []
+scripts = []
 inside_code = False
+script = []
+n = 0
 with open(args.input) as f:
     for line in f:
+        n += 1
         if line.startswith("```"):
+            if line[3:].rstrip() not in ("", "sh", "bash"):
+                raise ValueError(
+                    f"Line {n}: Only bash code blocks are supported: {line}"
+                )
             inside_code = not inside_code
+            if not inside_code:
+                scripts.append("".join(script))
+                script = []
         elif inside_code:
-            line = line.strip()
             for sub in args.sub:
                 find, replace = sub.split("=", 1)
                 line = line.replace(find, replace)
-            code.append(line)
+            script.append(line)
 
-for c in code:
-    print(f"Running {c}", flush=True)
+if script:
+    raise ValueError(f"Line {n}: Incomplete script: Missing closing ```")
+
+for s in scripts:
+    if CI:
+        firstline = s.strip().splitlines()[0]
+        print(f"::group::{firstline}")
+
+    print(f"Running\n```\n{s}\n```", flush=True)
     if args.run:
-        run(c, shell=True, check=True)
+        run(["bash", "-o", "errexit", "-o", "xtrace", "-c", s], check=True)
+
+    if CI:
+        print("::endgroup::")
